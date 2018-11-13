8caef96dbfb9be5bbb5e9f140523c42a10ead5c7
Kelson Teixeira <txkelson@gmail.com>
2018-11-13 03:55:41 -0200

    responsibilidade

---
+++ script.js
@@ -0,0 +1,9 @@
+var menu = document.getElementsByClassName("this_icon");
+var navegacao = document.getElementsByClassName("res_menu");
+var a = 0;
+
+
+
+menu.addEventListener ("click", function(){
+        navegacao.style.display = "block";
+})
